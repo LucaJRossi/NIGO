5163ac46f2960a34328b748ba65e194c2b6061c2
LucaJRossi <lucarossi@swin.edu.au>
2015-01-05 12:56:38 +1100

    Update compile_NIGO.sh

--- Scripts/compile_NIGO.sh
+++ Scripts/compile_NIGO.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # 
+#%%%%%%%%%% USER: EDIT THE PATH TO YOUR WORKING DIRECTORIES HERE %%%%%%%%
+# Make sure that the mkmf file is in the same directory of the source codes
+cd \path\to\source\codes
 rm makefile
 # 
 # Create the makefile
